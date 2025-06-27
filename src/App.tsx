2d38c538530d806bacfa11a232ebfb3ec7e05946
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-06-27 15:04:51 +0900

    Feat : 로그인 초기 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./Router";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import LoginPage from "./pages/LoginPage";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyles />
+    <LoginPage />
     <Router />
   </StrictMode>
 );
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -4,9 +4,14 @@ export const GlobalStyles = createGlobalStyle`
   p {
     margin: 0;
   }
+  body{
+    margin: 0;
+    padding: 0;
+    }
   #root {
     // to-do: 디자인 나오면 바꾸기
     max-width:800px;
     margin: 0 auto;
+    padding: 0;
   } 
 `;
