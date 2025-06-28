e47da565d8cb7aacf9994dd6f4b17062537c84cc
sooa@naver.com <leesooa0316@naver.com>
2025-06-28 18:01:25 +0900

    Feat: 라우터 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
+      element: <Home />,
     },
   ]);
   return <RouterProvider router={router} />;
