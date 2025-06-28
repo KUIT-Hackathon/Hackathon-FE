2c774f6e772d85c8e33b881acc08949e19b35bbc
sooa@naver.com <leesooa0316@naver.com>
2025-06-29 01:20:46 +0900

    Feat: 모달 추가

--- src/pages/paper/WritePaper.tsx
+++ src/pages/paper/WritePaper.tsx
@@ -1,79 +1,16 @@
-import styled from 'styled-components';
 import Header from '../../components/Header';
 import birthday from '../../assets/icon/paper/birthday.svg';
 import PurpleButton from '../../components/PurpleButton';
-import { createRef, useRef, useState } from 'react';
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  background: #f8f8f8;
-  padding: 32px 16px;
-  height: 100%;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 64px;
-  gap: 10px;
-  border-radius: 10px;
-  background: #fff;
-`;
-
-const InfoTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-`;
-
-const InfoTitle = styled.span`
-  color: var(--Grey-800, #2b2b2b);
-  font-size: 20px;
-  font-weight: 600;
-`;
-
-const InfoDesc = styled.span`
-  color: var(--Grey-400, #919191);
-  font-size: 14px;
-  font-weight: 600;
-`;
-
-const InputName = styled.input`
-  padding: 16px 22px;
-  border-radius: 16px;
-  background: #fff;
-  font-size: 16px;
-  font-weight: 600;
-  &::placeholder {
-    color: var(--Grey-250, #bebebe);
-  }
-`;
-
-const InputContent = styled.textarea`
-  padding: 27px 22px;
-  border-radius: 16px;
-  background: #fff;
-  font-size: 16px;
-  font-weight: 600;
-  height: 385px;
-  box-sizing: border-box;
-  &::placeholder {
-    color: var(--Grey-250, #bebebe);
-  }
-`;
+import { createRef, useState } from 'react';
+import Modal from '../../components/Modal';
+import airplane from '../../assets/icon/modal/airplane.svg';
+import { Wrapper, Container, InfoContainer, InfoTextContainer, InfoTitle, InfoDesc, InputName, InputContent } from './WritePaper.styles';
 
 const WritePaper = () => {
   const titleInputRef = createRef<HTMLInputElement | null>();
   const contentInputRef = createRef<HTMLTextAreaElement | null>();
   const [isValid, isSetValid] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const handleSubmit = () => {
     const title = titleInputRef.current?.value.trim();
     const content = contentInputRef.current?.value.trim();
@@ -104,6 +41,14 @@ const WritePaper = () => {
         <PurpleButton onClick={() => handleSubmit()} disabled={!isValid}>
           작성하기
         </PurpleButton>
+        <Modal
+          isOpen={isModalOpen}
+          icon={<img src={airplane} alt="체크 아이콘" />}
+          title="회원가입 완료"
+          description={`로그인 후 ToYou를\n이용하세요!`}
+          confirmText="확인"
+          onClose={() => setIsModalOpen(false)}
+        />
       </Container>
     </Wrapper>
   );
