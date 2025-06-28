65fdfaa4a91bf98920c07e7cc1202b18506c84b0
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-06-28 20:45:01 +0900

    Fix : login / signin 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/home/Home';
+import SignPage from './pages/login/SignPage';
+import LoginPage from './pages/login/LoginPage';
 import WritePaper from './pages/pager/WritePaper';
+
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -12,6 +15,14 @@ const Router = () => {
       path: '/paper/write',
       element: <WritePaper />,
     },
+    {
+      path: '/signin',
+      element:<SignPage/>
+    },
+    {
+      path: '/login',
+      element:<LoginPage/>
+    }
   ]);
   return <RouterProvider router={router} />;
 };
