e2c1a26e76ccebbd432011b557657e1d20cc864e
sooa@naver.com <leesooa0316@naver.com>
2025-06-28 14:12:23 +0900

    Style: Pretendard 폰트 추가

---
+++ src/styles/Fonts.tsx
@@ -0,0 +1,5 @@
+import { css } from 'styled-components';
+
+export const fonts = css`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
+`;
