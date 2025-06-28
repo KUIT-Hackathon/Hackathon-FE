221dd90ae0bf7f1eb922b84969753e643764df6c
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-06-28 22:30:01 +0900

    Style : prettier 적용

--- src/components/alarm/NotiFriend.tsx
+++ src/components/alarm/NotiFriend.tsx
@@ -1,58 +1,56 @@
-import styled from "styled-components";
-import friendIcon from "../../assets/icon/alarm/friendIcon.svg"
-
+import styled from 'styled-components';
+import friendIcon from '../../assets/icon/alarm/friendIcon.svg';
 
 const TextContainer = styled.div`
+  display: flex;
+  width: 338px;
+  height: 49px;
+  justify-content: left;
+  align-items: center;
+  flex-direction: row;
+  background-color: #e4d9ff;
+  border-radius: 5px;
+  padding: 5px 10px 5px 9px;
+  box-sizing: border-box;
+  gap: 4px;
+  img {
+    width: 24px;
+    height: 24px;
     display: flex;
-    width: 338px;
-    height: 49px;
-    justify-content:left;
-    align-items:center;
-    flex-direction: row;
-    background-color:#e4d9ff;
-    border-radius:5px;
-    padding : 5px 10px 5px 9px;
-    box-sizing: border-box;
-    gap:4px;
-    img{
-        width:24px;
-        height:24px;
-        display:flex;
-        align-items:center;
-        justify-content:center;
-    }
-    p{
-        display: flex;
-        width:250px;
-        align-items:center;
-        justify-content:left;
-        color:#454545;
-        font-size: 16px;
-        font-weight:600;
-        padding : 4px 10px;
-    }
-    button{
-        width:48px;
-        height:30px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #9C78EF;
-        border-radius: 12px;
-        color:#fff;
-        font-size: 14px;
-        font-weight: 600;
-    }
+    align-items: center;
+    justify-content: center;
+  }
+  p {
+    display: flex;
+    width: 250px;
+    align-items: center;
+    justify-content: left;
+    color: #454545;
+    font-size: 16px;
+    font-weight: 600;
+    padding: 4px 10px;
+  }
+  button {
+    width: 48px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #9c78ef;
+    border-radius: 12px;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+  }
 `;
 
-
-const NotiFriend = ({friend}) =>{
-    return(
-            <TextContainer>
-                <img src={friendIcon} alt="팔로우 알림"/>
-                <p>{friend} 님이 나를 팔로우 했어요!</p>
-            </TextContainer>
-    );
+const NotiFriend = ({ friend }) => {
+  return (
+    <TextContainer>
+      <img src={friendIcon} alt="팔로우 알림" />
+      <p>{friend} 님이 나를 팔로우 했어요!</p>
+    </TextContainer>
+  );
 };
 
 export default NotiFriend;
--- src/pages/alarm/AlarmPage.tsx
+++ src/pages/alarm/AlarmPage.tsx
@@ -1,71 +1,68 @@
-import styled from "styled-components";
-import NotiPaper from "../../components/alarm/NotiPaper"
-import NotiFriend from "../../components/alarm/NotiFriend"
-import Header from "../../components/Header"
-
+import styled from 'styled-components';
+import NotiPaper from '../../components/alarm/NotiPaper';
+import NotiFriend from '../../components/alarm/NotiFriend';
+import Header from '../../components/Header';
 
 const Container = styled.div`
-    width: 375px;
-    height: 812px;
-    display: flex;
-    justify-content: flex-start ;
-    align-items: center ;
-    flex-direction: column;
-    background-color: #f8f8f8;
-    gap: 28px;
+  width: 375px;
+  height: 812px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f8f8f8;
+  gap: 28px;
 `;
 
-
 const MsgContainer = styled.div`
-    width:338px;
-    display: flex;
-    align-items: center;
-    justify-content : center;
-    flex-direction: column;
-    gap: 10px;
+  width: 338px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 const DateWrapper = styled.div`
-    display: flex;
-    align-items: left;
-    justify-content: center;
-    flex-direction: column;
-    gap : 15px;
-    h3{
-        color: #2b2b2b;
-        font-size: 18px;
-        font-weight: 600;
-    }
+  display: flex;
+  align-items: left;
+  justify-content: center;
+  flex-direction: column;
+  gap: 15px;
+  h3 {
+    color: #2b2b2b;
+    font-size: 18px;
+    font-weight: 600;
+  }
 `;
 
 const AlarmPage = () => {
-    return (
-        <Container>
-            <Header title="소식함" />
-            <DateWrapper>
-                <h3>2025.06.23.</h3>
-                <MsgContainer>
-                    <NotiPaper friend="태성" />
-                    <NotiFriend friend="태성" />
-                </MsgContainer>
-            </DateWrapper>
-            <DateWrapper>
-                <h3>2025.06.22.</h3>
-                <MsgContainer>
-                    <NotiPaper friend="태성" />
-                    <NotiFriend friend="태성" />
-                </MsgContainer>
-            </DateWrapper>
-            <DateWrapper>
-                <h3>2025.06.21.</h3>
-                <MsgContainer>
-                    <NotiPaper friend="태성" />
-                    <NotiFriend friend="태성" />
-                </MsgContainer>
-            </DateWrapper>
-        </Container>
-
-    );
+  return (
+    <Container>
+      <Header title="소식함" />
+      <DateWrapper>
+        <h3>2025.06.23.</h3>
+        <MsgContainer>
+          <NotiPaper friend="태성" />
+          <NotiFriend friend="태성" />
+        </MsgContainer>
+      </DateWrapper>
+      <DateWrapper>
+        <h3>2025.06.22.</h3>
+        <MsgContainer>
+          <NotiPaper friend="태성" />
+          <NotiFriend friend="태성" />
+        </MsgContainer>
+      </DateWrapper>
+      <DateWrapper>
+        <h3>2025.06.21.</h3>
+        <MsgContainer>
+          <NotiPaper friend="태성" />
+          <NotiFriend friend="태성" />
+        </MsgContainer>
+      </DateWrapper>
+    </Container>
+  );
 };
 
 export default AlarmPage;
